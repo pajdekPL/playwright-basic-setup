ef06f8f5d40a6476c0e98fd44848c9dec26a7726
PajdekPL <adam_pajda@outlook.com>
2025-03-20 20:06:58 +0100

    fix: setup comments

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  // 1) add to spaces before page and and click ctrl+save and check if auto format is working
+  // 2) if it works let's check why we have this red error in the line 6(it should show error if not something is wrong with our setup)
   page.goto('https://playwright.dev/');
-  // Expect a title "to contain" a substring.
 });
 
 test('get started link', async ({ page }) => {
+  // unused variable also should be highlighted
   const someUnusedConst = "I'm not used";
   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
   await expect(
     page.getByRole('heading', { name: 'Installation' }),
   ).toBeVisible();
